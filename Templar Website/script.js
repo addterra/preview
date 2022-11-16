582e4f2f208002dc3a7349cfd5255f97e2ae0dee
addterra <98066696+addterra@users.noreply.github.com>
2022-11-16 11:18:43 -0700

    Add files via upload

--- Templar Website/script.js
+++ Templar Website/script.js
@@ -11,3 +11,26 @@ function in40s() {
 function gif() {
     document.querySelector(".logo_gif").src = "images/logo gif.gif"; 
 }
+
+
+// activate title animation when the title is on screen 
+
+window.onload = main;
+
+function main() {
+    window.addEventListener("scroll", function(){
+        if(isElementIntoView("#about")) document.querySelector("#title1").style.animationName = "left"; 
+    });
+}
+
+function title2() {
+    window.addEventListener("scroll", function(){
+        if(isElementIntoView("#services")) document.querySelector("#title2").style.animationName = "right";
+    });
+}
+
+function isElementIntoView(el) { 
+    var e = document.querySelector(el);
+    var rect = e.getBoundingClientRect();
+    return  window.innerHeight-rect.top >= 0; 
+}
